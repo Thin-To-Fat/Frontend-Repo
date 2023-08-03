0072f1010e2db119eb8d7745353d25363422a00c
parksangjun1196 <shpsjpsj9611@gamil.com>
2023-08-03 23:47:39 +0900

    0803 상단 최대지출일,일평균지출,지출,수입 API연결

--- src/pages/transaction/Transaction.js
+++ src/pages/transaction/Transaction.js
@@ -3,7 +3,7 @@ import './Transaction.scss';
 import Template from '../../component/Template'
 
 import faker from 'faker';
-import React, { useState  } from 'react';
+import React, { useState, useEffect  } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -57,10 +57,41 @@ export const data = {
   ],
 };
 
+// var beverage = (age >= 21) ? "Beer" : "Juice";
 
+// eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTY5MTA3MDkwMSwiZXhwIjoxNjkxMDg4OTAxfQ.HAurDpUF7bVenTrG7bHRqdAdqz1uZHiaNNufXwIN7Xc
 function Transaction() {
   const [isToggled, setIsToggled] = useState(false);
   const [isToggled2, setIsToggled2] = useState(false);
+  const [maxOutDay, setMaxOutDay] = useState("0");
+  const [monthIn, setMonthIn] = useState(0);
+  const [monthOut, setMonthOut] = useState(0);
+  const [dayAvgIn, setDayAvgIn] = useState(0);
+  const addComma = (price) => {
+    let returnString = price?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return returnString;
+}
+  useEffect(()=> {
+    var nowdate = new Date();
+    var month = (nowdate.getMonth()+1 < 10) ? "0"+(nowdate.getMonth()+1) : nowdate.getMonth()+1 + "";
+
+    var nowdatestr= nowdate.getUTCFullYear() + "-" + month
+
+    var fetchaddr = 'http://localhost:7070/api/v1/history/info?nowdate=' + nowdatestr
+
+    fetch(fetchaddr, {
+        method : "GET",
+        headers: {
+          'X-AUTH-TOKEN': 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTY5MTA3MDkwMSwiZXhwIjoxNjkxMDg4OTAxfQ.HAurDpUF7bVenTrG7bHRqdAdqz1uZHiaNNufXwIN7Xc',
+        }  
+    }).then(res=>res.json()).then(res=>{
+        console.log(1, res);
+        setMaxOutDay(res.result[0].maxDay)
+        setMonthOut(addComma(res.result[0].monthChange))
+        setMonthIn(addComma(res.result[0].monthIncome))
+        setDayAvgIn(addComma(res.result[0].dayChangeAvg))
+    });              
+}, []);
 
   const handleToggleColor = () => {
     setIsToggled(!isToggled);
@@ -86,14 +117,14 @@ function Transaction() {
                         <div className='TXouttoggle' onClick={handleToggleColor} style={{ backgroundColor: isToggled ? '#ED6C6E' : 'white' , borderColor : isToggled ? '#ED6C6E':'#E6E6E6' }}>
                           <img src='/images/Txarrow.png'></img>
                         </div>
-                        <div className="TXoutval" style={{ color: isToggled ? '#ED6C6E' : '#6D6D6D' } }>1,049,600원</div>
+                        <div className="TXoutval" style={{ color: isToggled ? '#ED6C6E' : '#6D6D6D' } }>{monthOut}원</div>
                       </div>
                       <div className='TXouttext'>수입</div>
                       <div className='TXintogglebox'>
                         <div className='TXintoggle' onClick={handleToggleColor2} style={{ backgroundColor: isToggled2 ? '#ED6C6E' : 'white', borderColor : isToggled2 ? '#ED6C6E':'#E6E6E6' }}>
                           <img src='/images/Txarrow.png'></img>
                         </div>
-                        <div className='TXinval' style={{ color: isToggled2 ? '#ED6C6E' : '#6D6D6D' }}>2,650,000원</div>
+                        <div className='TXinval' style={{ color: isToggled2 ? '#ED6C6E' : '#6D6D6D' }}>{monthIn}원</div>
                       </div>
                     </div>
                     <div className='TXTopleftcontentgraph'><Line options={options} data={data} /></div>
@@ -101,11 +132,11 @@ function Transaction() {
                 <div className='TXToprightcontent'>
                     <div className='TXmaxout'>
                       <p>최대 지출일</p>
-                      <p className='TXmaxoutnum'>10일</p>
+                      <p className='TXmaxoutnum'>{maxOutDay}일</p>
                     </div>
                     <div className='TXdayavgout'>
                       <p>일 평균 지출</p>
-                      <p className='TXdayavgoutnum'>33,588원</p>
+                      <p className='TXdayavgoutnum'>{dayAvgIn}원</p>
                       
                     </div>
                 </div>
