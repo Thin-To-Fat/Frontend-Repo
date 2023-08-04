76b9714b3dddba2f35f0edbb9544df736bb5f940
icekjm <icekjm1029@gmail.com>
2023-08-04 14:33:08 +0900

    feat: 라이브러리 api 불러오기

--- src/pages/library/Library.js
+++ src/pages/library/Library.js
@@ -11,19 +11,16 @@ function LibraryJM() {
   const [stanbank, setStanbank] = useState([]);
 
   useEffect(() => {
-    console.log(localStorage.getItem('X-AUTH-TOKEN'));
     axios
-      .post('/api/v1/library/totalacc', {
+      .get('/api/v1/library/totalacc', {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem('X-AUTH-TOKEN')}`,
-          // 'X-AUTH-TOKEN': localStorage.getItem('X-AUTH-TOKEN'),
+          'Content-Type': 'application/json',
+          'X-AUTH-TOKEN': localStorage.getItem('X-AUTH-TOKEN'),
         },
       })
       .then((response) => {
-        // localStorage.setItem('X-AUTH-PATH', JSON.stringify(response.data.result));
-        // localStorage.setItem('X-AUTH-TOKEN', response.data.result);
-        // navigate('/dashboard');
-        setStanbank('X-AUTH-TOKEN', response.data.result);
+        setStanbank(response.data.result);
+        // console.log(response.data.result);
       });
   }, []);
 
@@ -35,17 +32,13 @@ function LibraryJM() {
           <div className="wholeFirst">
             <div className="firstLib">입출금계좌</div>
             <div className="firstLibbody">
+              {/* 입출금계좌길이체크 시작 */}
               <div>
                 <Swiper
                   className="secondSwiperb"
                   spaceBetween={83}
                   slidesPerView={3}
                   navigation={true}
-                  // breakpoints={{
-                  //   768: {
-                  //     slidesPerView: 7,
-                  //   },
-                  // }}
                 >
                   <SwiperSlide className="libBanks">
                     <div className="bankNick">
@@ -63,56 +56,6 @@ function LibraryJM() {
                       </div>
                     </div>
                   </SwiperSlide>
-                  <SwiperSlide className="libBanks">
-                    <div className="bankNick">
-                      <div className="eachBank">신한은행(1244)</div>
-                      <div className="eachNick">희망소망통장</div>
-                    </div>
-
-                    <div className="imgTypeAcc">
-                      <div className="eachBankImg">
-                        <img src="https://thin-to-fat.s3.ap-northeast-2.amazonaws.com/bankInfo/shinhan.png" />
-                      </div>
-                      <div className="typeAmount">
-                        <div>타입: 출금전용계좌</div>
-                        <div>이번달 사용금액: 100,000원</div>
-                        <div>잔액: 500,000원</div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-
-                  <SwiperSlide className="libBanks">
-                    <div className="bankNick">
-                      <div className="eachBank">우리은행(5748)</div>
-                      <div className="eachNick">정해나의 보물창고</div>
-                    </div>
-
-                    <div className="imgTypeAcc">
-                      <div className="eachBankImg">
-                        <img src="https://thin-to-fat.s3.ap-northeast-2.amazonaws.com/bankInfo/woori.png" />
-                      </div>
-                      <div className="typeAmount">
-                        <div>타입: 계좌</div>
-                        <div>잔액: 2,200,000원</div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide className="libBanks">
-                    <div className="bankNick">
-                      <div className="eachBank">시티은행(1231)</div>
-                      <div className="eachNick">김영준짱의 계좌</div>
-                    </div>
-
-                    <div className="imgTypeAcc">
-                      <div className="eachBankImg">
-                        <img src="https://thin-to-fat.s3.ap-northeast-2.amazonaws.com/bankInfo/citi.png" />
-                      </div>
-                      <div className="typeAmount">
-                        <div>타입: 계좌</div>
-                        <div>잔액: 6,200,000원</div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
                 </Swiper>
               </div>
             </div>
@@ -127,11 +70,6 @@ function LibraryJM() {
                   spaceBetween={83}
                   slidesPerView={3}
                   navigation={true}
-                  // breakpoints={{
-                  //   768: {
-                  //     slidesPerView: 7,
-                  //   },
-                  // }}
                 >
                   <SwiperSlide className="libBanks">
                     <div className="bankNick">
@@ -146,55 +84,6 @@ function LibraryJM() {
                       <div className="typeAmount">
                         <div>타입: 적금</div>
                         <div>잔액: 800,000원</div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide className="libBanks">
-                    <div className="bankNick">
-                      <div className="eachBank">농협은행(2311)</div>
-                      <div className="eachNick">청년주택청약적금</div>
-                    </div>
-
-                    <div className="imgTypeAcc">
-                      <div className="eachBankImg">
-                        <img src="https://thin-to-fat.s3.ap-northeast-2.amazonaws.com/bankInfo/nonghyub.png" />
-                      </div>
-                      <div className="typeAmount">
-                        <div>타입: 주택청약</div>
-                        <div>잔액: 1,530,000원</div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-
-                  <SwiperSlide className="libBanks">
-                    <div className="bankNick">
-                      <div className="eachBank">우리은행(5748)</div>
-                      <div className="eachNick">노후완전대비적금</div>
-                    </div>
-
-                    <div className="imgTypeAcc">
-                      <div className="eachBankImg">
-                        <img src="https://thin-to-fat.s3.ap-northeast-2.amazonaws.com/bankInfo/woori.png" />
-                      </div>
-                      <div className="typeAmount">
-                        <div>타입: 적금</div>
-                        <div>잔액: 2,800,000원</div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide className="libBanks">
-                    <div className="bankNick">
-                      <div className="eachBank">시티은행(1231)</div>
-                      <div className="eachNick">2030을 위한 MZ적금</div>
-                    </div>
-
-                    <div className="imgTypeAcc">
-                      <div className="eachBankImg">
-                        <img src="https://thin-to-fat.s3.ap-northeast-2.amazonaws.com/bankInfo/citi.png" />
-                      </div>
-                      <div className="typeAmount">
-                        <div>타입: 적금</div>
-                        <div>잔액: 1,250,000원</div>
                       </div>
                     </div>
                   </SwiperSlide>
